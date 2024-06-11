dfac92a7e05fda25e25842992430fcf39ebe0f5a
mzdih <mehdi.zdih@qytera.de>
2024-06-11 14:59:33 +0200

    use chrome remote

--- src/test/java/de/syneco/TestOne.java
+++ src/test/java/de/syneco/TestOne.java
@@ -1,23 +1,55 @@
 package de.syneco;
 
-import de.qytera.qtaf.core.config.annotations.TestFeature;
-import de.qytera.qtaf.testng.context.QtafTestNGContext;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-@TestFeature(
-        name = "TestCase One",
-        description = "First test"
-)
-public class TestOne extends QtafTestNGContext {
-    @Test(testName = "Test download file")
+public class TestOne {
+
+    private static final String SELENIUM_HUB_URL = "http://localhost:4444/wd/hub";
+
+    public static WebDriver remoteChromeDriver() throws MalformedURLException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        return new RemoteWebDriver(new URL(SELENIUM_HUB_URL), options);
+    }
+
+    public static WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        return new ChromeDriver(options);
+    }
+
+    @Test
     public void download() {
-        System.out.println("TEST BEGIN");
-        DownloadPage downloadPage = load(DownloadPage.class);
-        driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html");
-        sleep(9000);
-        downloadPage.simpleDocButton().click();
-        System.out.println("TEST END");
+        WebDriver driver = null;
+        try {
+            System.out.println("TEST BEGIN");
+            driver = remoteChromeDriver();
+            driver.get("https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html");
+            Thread.sleep(9000); // Consider using WebDriverWait instead of Thread.sleep for better stability.
+            driver.findElement(By.xpath("//a[text()='airtravel.csv']")).click();
+            System.out.println("TEST END");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
     }
 }
