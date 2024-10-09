c5060f3252b5fb73414ad9a8ab597c7c0ec9c1ed
Oliver Hönig <oliver-hoenig@gmx.de>
2024-10-09 12:02:01 +0200

    test: add helpers to modify driver options

--- src/test/java/de/syneco/TestOne.java
+++ src/test/java/de/syneco/TestOne.java
@@ -3,6 +3,7 @@ package de.syneco;
 import de.qytera.qtaf.core.config.ConfigurationFactory;
 import de.qytera.qtaf.core.config.annotations.TestFeature;
 import de.qytera.qtaf.core.io.DirectoryHelper;
+import de.qytera.qtaf.core.selenium.DriverFactory;
 import de.qytera.qtaf.testng.context.QtafTestNGContext;
 import org.openqa.selenium.HasDownloads;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,7 +13,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
@@ -21,9 +25,48 @@ import static com.codeborne.selenide.Selenide.sleep;
         description = "First test"
 )
 public class TestOne extends QtafTestNGContext {
+
+    public static String addDriverOptionArgument(String newArgument, String currentArguments){
+        currentArguments = currentArguments.replace("[", "").replace("]", "");
+
+        List<String> argumentList = new ArrayList<>(Arrays.asList(currentArguments.split(",")));
+        argumentList.add(newArgument.trim());
+
+        String result = String.join(", ", argumentList);
+        return "[" + result + "]";
+    }
+
+    public static String removeAnyArgumentsThatIncludesHead(String arguments) {
+        arguments = arguments.replace("[", "").replace("]", "");
+
+        List<String> argumentList = Arrays.asList(arguments.split(","));
+        List<String> filteredArguments = argumentList.stream()
+                .map(String::trim)  // Trim any extra spaces
+                .filter(arg -> !arg.contains("head"))
+                .collect(Collectors.toList());
+
+        String result = String.join(", ", filteredArguments);
+        return "[" + result + "]";
+    }
+
     @Test(testName = "Test download file")
     public void download() throws IOException {
         System.out.println("TEST BEGIN");
+
+        // Configure Driver Options
+        String driverOptions = config.getValue("driver.options").toString();
+        System.out.println("Old Options: " + driverOptions);
+
+        driverOptions = removeAnyArgumentsThatIncludesHead(driverOptions);
+        driverOptions = addDriverOptionArgument("--headless", driverOptions);
+        config.setString("driver.options", driverOptions);
+
+        // Rest Driver
+        DriverFactory.getDriver(true);
+
+        driverOptions = config.getValue("driver.options").toString();
+        System.out.println("New Options: " + driverOptions);
+
 
         if(driver.getClass() == RemoteWebDriver.class) {
             assertTrue(((HasDownloads) driver).getDownloadableFiles().isEmpty(), "There should be no files downloaded");
